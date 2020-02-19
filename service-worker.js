71b2ab760cc7db79650c50482850d6ab8c3a81fd
Luiz <lf1405@hotmail.com>
2020-02-19 17:30:19 -0300

    manifest e service worker

---
+++ service-worker.js
@@ -0,0 +1,45 @@
+const cacheName = "v1";
+const cacheAssets = [
+  "index.html",
+  "styles.css",
+  "curriculo.pdf",
+  "/image/aigree_logo.png",
+  "/image/fidelizapp_logo.png",
+  "/image/spot_logo.jpg",
+  "/image/pingui_logo.png"
+];
+
+// Call Install Event
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches
+      .open(cacheName)
+      .then(cache => {
+        cache.addAll(cacheAssets);
+      })
+      .then(() => {
+        self.skipWaiting();
+      })
+  );
+});
+
+// Call activate Event
+self.addEventListener("activate", e => {
+  // Remove unwanted caches
+  e.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== cacheName) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+//Call fetch Event
+self.addEventListener("fetch", e => {
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
